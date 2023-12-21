4ebea5aab172e6530fc692cc2d7ccca2277005b8
GgAccel <hoanggiang01029@gmail.com>
2023-12-22 00:20:22 +0700

    Hoe Ground

--- Assets/FarmingGame/Scripts/Player/PlayerInteract.cs
+++ Assets/FarmingGame/Scripts/Player/PlayerInteract.cs
@@ -1,10 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] private Collider2D hit;
     public ItemType HoldItemType { get; private set; } = ItemType.None;
+    private InventoryData InventoryData => InventoryData.Instance;
+    private Transform _cacheTransform;
+
+
+    private void Start()
+    {
+        _cacheTransform = transform;
+    }
 
     public void OnChangeItemHold(ItemType itemType)
     {
@@ -15,7 +24,16 @@ public class PlayerInteract : MonoBehaviour
 
     public void Interact()
     {
-        hit.gameObject.SetActive(true);
+        switch (InventoryData.CurrentHoldItem.Config.Type)
+        {
+            case ItemType.Axe:
+                hit.gameObject.SetActive(true);
+                break;
+            case ItemType.Hoe:
+                var currentPos = _cacheTransform.position;
+                TileManager.SetTileHoed(new Vector3Int((int)currentPos.x, (int)currentPos.y));
+                break;
+        }
     }
 
     public void DoneInteract()
--- Assets/FarmingGame/Scripts/Tile/TileManager.cs
+++ Assets/FarmingGame/Scripts/Tile/TileManager.cs
@@ -2,11 +2,14 @@ using System;
 using GgAccel;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class TileManager : MonoSingleton<TileManager>
 {
     [SerializeField] private Tilemap interactableMap;
     [SerializeField] private Tile hiddenInteractableTile;
+    [SerializeField] private AdvancedRuleTile tileDirt;
+    [SerializeField] private AudioClip[] sfxHoes;
 
     private void Start()
     {
@@ -20,7 +23,22 @@ public class TileManager : MonoSingleton<TileManager>
         }
     }
 
-    public bool IsTileInteractable(Vector3Int pos)
+    public static void SetTileHoed(Vector3Int pos)
+    {
+        Instance._SetTileHoed(pos);
+    }
+
+    private void _SetTileHoed(Vector3Int pos)
+    {
+        if (IsTileInteractable(pos))
+        {
+            interactableMap.SetTile(pos, tileDirt);
+            interactableMap.SetColor(pos, Color.white);
+            AudioManager.PlaySound(sfxHoes[Random.Range(0, sfxHoes.Length)]);
+        }
+    }
+
+    private bool IsTileInteractable(Vector3Int pos)
     {
         var tile = interactableMap.GetTile(pos);
         return tile;
